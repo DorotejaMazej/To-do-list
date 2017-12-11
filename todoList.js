4cfe8697b0500a453da4b03ab59a7b803171ecd4
DorotejaMazej <DorotejaMazej@users.noreply.github.com>
2017-12-11 13:27:44 +0100

    Add files via upload

---
+++ todoList.js
@@ -0,0 +1,60 @@
+//check li 
+//$("li").click(function(){
+//	if($(this).css("color") === "rgb(128, 128, 128)"){
+//		$(this).css({
+//			color: "black", 
+//			textDecoration: "none"
+//		})
+//	}else{
+//		$(this).css({
+//			color: "gray",
+//			textDecoration: "line-through"
+//		})
+//	}
+//})
+
+// toggle done comments
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed")
+});
+
+//delete todos
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//create new todos
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash' ariaHidden= 'true'></i></span>" + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
